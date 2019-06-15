dadb4af68d7b24abe415d5c05560cdca928c6afd
slowmoon <slowmoon@qq.com>
2019-06-14 23:22:50 -0700

    get local ip with filter

--- network/ip/ip.go
+++ network/ip/ip.go
@@ -15,3 +15,19 @@ func  GetLocalIp()([]net.IP, error)  {
     }
     return  ips, nil
 }
+
+func GetLocalIpWithFilter(f func(net.IP)bool)([]net.IP, error) {
+    var ips []net.IP
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return  nil, err
+    }
+    for _, addr := range  addrs {
+        if ip, ok := addr.(*net.IPNet);ok &&  f(ip.IP){
+            ips = append( ips, ip.IP.To4())
+        }
+    }
+    return  ips, nil
+}
+
+
